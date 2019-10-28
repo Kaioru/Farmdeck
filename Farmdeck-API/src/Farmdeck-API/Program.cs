6b62736acfc31b913c03d053970b4bee29864d87
bokyan <kohbokyan@gmail.com>
2019-10-28 23:13:51 +0800

    Update Program.cs

--- Farmdeck-API/src/Farmdeck-API/Program.cs
+++ Farmdeck-API/src/Farmdeck-API/Program.cs
@@ -7,14 +7,50 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using MQTTnet;
 using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Client;
 using MQTTnet.Client.Options;
 
 namespace Farmdeck_API
 {
    class Program
     {
+        static IManagedMqttClient client = new MqttFactory().CreateManagedMqttClient();
         static void Main(string[] args)
         {
+
+            // send message
+            Task message = PublishAsync("allahu", "akbar");
+
+            // receiving message
+            if (client.IsConnected)
+            {
+                client.UseApplicationMessageReceivedHandler(e =>
+                {
+                    try
+                    {
+                        string topic = e.ApplicationMessage.Topic;
+
+                        if (string.IsNullOrWhiteSpace(topic) == false)
+                        {
+                            string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                            Console.WriteLine($"Topic: {topic}. Message Received: {payload}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message, ex);
+                    }
+                });
+            }
+
+            Program prog = new Program();
+
+        }
+
+        Program()
+        { 
+            // connecting
+            Task connectAsync = ConnectAsync();
         }
 
         public static async Task ConnectAsync()
@@ -25,6 +61,7 @@ namespace Farmdeck_API
             string mqttPassword = "3kZymesJiHWk";
             int mqttPort = 14103;
             bool mqttSecure = false;
+
             var messageBuilder = new MqttClientOptionsBuilder()
                 .WithClientId(clientId)
                 .WithCredentials(mqttUser, mqttPassword)
@@ -37,12 +74,14 @@ namespace Farmdeck_API
                 .Build()
               : messageBuilder
                 .Build();
+
+
             var managedOptions = new ManagedMqttClientOptionsBuilder()
               .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
               .WithClientOptions(options)
               .Build();
 
-            var client = new MqttFactory().CreateManagedMqttClient();
+
             await client.StartAsync(managedOptions);
         }
 
