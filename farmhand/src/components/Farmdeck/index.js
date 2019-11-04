74bb3f25b703ab761a4bd1e4d3b07984bb5e5efa
bokyan <kohbokyan@gmail.com>
2019-11-04 22:10:43 +0800

    Add axios dependency

---
+++ farmhand/src/components/Farmdeck/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Farmdeck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(
+      result => {
+        this.setState({
+          isLoaded: true,
+          items: result.data
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
