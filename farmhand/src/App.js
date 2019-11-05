1ea4e7fdd655c912ba49b6b0e18db3187d579fd7
bokyan <kohbokyan@gmail.com>
2019-11-05 20:59:31 +0800

    Revert "Add styling to App.js"
    This reverts commit 4d1df0e67ffd744853a443b3312a0c243f020384.

--- farmhand/src/App.js
+++ farmhand/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Sidebar, SidebarItem } from "react-rainbow-components";
 import dashboard from "./assets/icons/dashboard.svg";
 import charts from "./assets/icons/charts.svg";
-import "./styles.css";
 
 class App extends Component {
   constructor(props) {
@@ -20,19 +19,9 @@ class App extends Component {
   render() {
     const { selectedItem } = this.state;
     return (
-      <div id="cardContainer">
-        <div id="headerContainer">
-          <header className="rainbow-align-content_space-between rainbow-p-vertical_small react-rainbow-golbal-header rainbow-background-color_white">
-            <div className="rainbow-flex rainbow-align_center">
-              <p>FarmHand</p>
-            </div>
-          </header>
-        </div>
+      <div>
         <header className="rainbow-align-content_space-between rainbow-background-color_white rainbow-p-vertical_medium react-rainbow-golbal-header"></header>
-        <div
-          className="rainbow-background-color_white rainbow-p-top_small rainbow-p-bottom_medium"
-          id="sidebarContainer"
-        >
+        <div>
           <Sidebar
             selectedItem={selectedItem}
             onSelect={this.handleOnSelect}
