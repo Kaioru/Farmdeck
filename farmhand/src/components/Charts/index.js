607f5cdde820336cefa1cde7317dfaa1b0b871f8
bokyan <kohbokyan@gmail.com>
2019-11-05 23:22:29 +0800

    Update charts component

---
+++ farmhand/src/components/Charts/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Charts extends Component {
+  render() {
+    return (
+      <div>
+        <p>You're in charts</p>
+      </div>
+    );
+  }
+}
