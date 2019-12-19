388625d51c502d7c4d00e924a52b75bb5c30c355
Kaioru <keith@kaioru.co>
2019-12-19 20:14:06 +0800

    Create design for deck voice control

--- farmhand_app/lib/app/pages/deck_voice.dart
+++ farmhand_app/lib/app/pages/deck_voice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import '../app_state.dart';
 
 class DeckVoice extends StatefulWidget {
@@ -13,12 +14,61 @@ class DeckVoice extends StatefulWidget {
 class _DeckVoice extends State<DeckVoice> {
   final AppState state;
 
+  bool recording = false;
+  String inputText = "";
+
   _DeckVoice(this.state);
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Container(),
-    );
+        child: SafeArea(
+      child: Container(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(inputText,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        )),
+                  ],
+                ),
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      recording
+                          ? "Go on, we're listening.."
+                          : "Tap the mic to start!",
+                      style: TextStyle(
+                          fontSize: 24.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 18.0),
+                    FloatingActionButton(
+                      backgroundColor:
+                          recording ? Colors.redAccent : Colors.blueAccent,
+                      child: Icon(Icons.mic),
+                      elevation: 0,
+                      onPressed: () {
+                        setState(() {
+                          recording = !recording;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
+    ));
   }
 }
