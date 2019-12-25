9968b0030f42a1d023121a6c9b071ff5f9921777
Kaioru <keith@kaioru.co>
2019-12-25 19:31:52 +0800

    Add more info to token contract

--- Homestead/src/Homestead.WebAPI/Contracts/TokenContract.cs
+++ Homestead/src/Homestead.WebAPI/Contracts/TokenContract.cs
@@ -4,6 +4,11 @@ namespace Homestead.WebAPI.Contracts
 {
     public class TokenContract
     {
+        public string DisplayName { get; set; }
+        public string DisplayPicture { get; set; }
+
+        public string Username { get; set; }
+
         public string Token { get; set; }
         public DateTime Expire { get; set; }
     }
